b60f2e9c43530cbb87b4c7405aa6a7ffebcfcd0e
Nick <nick@dawncreative.co.uk>
2020-10-27 13:49:36 +0000

    tidy up drag handling in state

--- src/App.js
+++ src/App.js
@@ -22,15 +22,44 @@ class App extends Component {
       puppy1Y: 0,
       puppy2X: 0,
       puppy2Y: 0,
+      items: {
+        puppy1: {
+          posX: 0,
+          poY: 0,
+          rotation: 0
+        },
+        puppy2: {
+          posX: 0,
+          poY: 0,
+          rotation: 0
+        }
+      },
       lines: [["line-start", "line-end"]]
     }
   }
 
-  handleDrag = (e, i) => {
-    const newState = {}
-    newState[`puppy${i}x`] = e.clientX
-    newState[`puppy${i}y`] = e.clientY
-    this.setState(newState)
+  handleDrag = (e, id) => {
+    this.renderLines();
+    this.setState((currentState) => {
+      const itemCopy = {...currentState.items[id]}
+      itemCopy.posX = e.clientX;
+      itemCopy.posY = e.clientY;
+      const newState = {items: {...currentState.items}}
+      newState.items[id] = itemCopy;
+      return newState;
+    })
+  }
+
+  handleDragStop = (e, id) => {
+    
+  }
+
+  renderLines = () => {
+    return this.state.lines.map(line => {
+      return (
+        <LineTo from={line[0]} to={line[1]} borderColor="red" borderStyle="solid" borderWidth="2"></LineTo>
+      )
+    })
   }
  
   render() {
@@ -41,27 +70,24 @@ class App extends Component {
        <h1>Puppy Board</h1>
 
           <Rotatable>
-        <Draggable onDrag={(e) => {this.handleDrag(e, 1)}}>
+        <Draggable onDrag={(e) => {this.handleDrag(e, "puppy1")}} onStop={(e) => {this.handleDragStop(e, "puppy1")}}>
           <div className="line-start">
-    <h3>{this.state.puppy1x}, {this.state.puppy1y}</h3>
+    <h3>{this.state.items.puppy1.posX}, {this.state.items.puppy1.posY}</h3>
+    <h3>{`${this.state.items.puppy1.rotation}deg`}</h3>
         </div>
         </Draggable>
           </Rotatable>
 
         <Rotatable>
-        <Draggable onDrag={(e) => {this.handleDrag(e, 2)}}>
+        <Draggable onDrag={(e) => {this.handleDrag(e, "puppy2")}} onStop={(e) => {this.handleDragStop(e, "puppy2")}}>
           <div className="line-end">
-          <h3>{this.state.puppy2x}, {this.state.puppy2y}</h3>
+          <h3>{this.state.items.puppy2.posX}, {this.state.items.puppy2.posY}</h3>
           </div>
         </Draggable>
         </Rotatable>
         
 
-        {this.state.lines.map(line => {
-          return (
-            <LineTo from={line[0]} to={line[1]} borderColor="red" borderStyle="solid" borderWidth="2"></LineTo>
-          )
-        })}
+        {this.renderLines()}
         
       </div>
       //   </TransformComponent>
