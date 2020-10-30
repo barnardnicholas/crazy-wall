e734f69c3c3c4ea1b4283b968a9fbbeecf5466d4
Nick <nick@dawncreative.co.uk>
2020-10-30 17:47:02 +0000

    make pictures into polaroids

--- src/components/item-content/PictureContent.js
+++ src/components/item-content/PictureContent.js
@@ -1,20 +1,28 @@
 import React from "react";
+import polaroidFrame from "../../assets/img/polaroid-frame.png";
+import "./Items.css";
 
 const PictureContent = (props) => {
   const { handleContentClick, handleMoveToFront, handleMoveToBack } = props;
-  const { id, name, imageUrl } = props.data;
+  const { id, name, width, height, imageUrl } = props.data;
   //   const { id, name } = props.data;
   return (
-    <div
-      className={`content content1 puppy`}
-      onClick={handleContentClick}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
-      {name}
+    <div className={`content content1`} onClick={handleContentClick}>
+      <div style={{ width, height }}>
+        <div
+          className="pic-polaroid bg-cover"
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        ></div>
+        <div
+          className="bg-cover pic-frame-polaroid"
+          style={{ backgroundImage: `url(${polaroidFrame})` }}
+        ></div>
+        {/* {name} */}
 
-      <div className="order-controls">
-        <button onClick={handleMoveToFront}>Bring to Front</button>
-        <button onClick={handleMoveToBack}>Move to Back</button>
+        <div className="order-controls">
+          <button onClick={handleMoveToFront}>F</button>
+          <button onClick={handleMoveToBack}>B</button>
+        </div>
       </div>
     </div>
   );
