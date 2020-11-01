c234a235bc027e36bbf353fe3d11eb9496c5164c
Nick <nick@dawncreative.co.uk>
2020-11-01 11:46:10 +0000

    add localStorage reset/backup, remove initial items state

--- src/App.js
+++ src/App.js
@@ -116,79 +116,7 @@ class App extends Component {
     this.state = {
       dataLoaded: false,
       activeItem: null,
-      items: [
-        {
-          type: "photo",
-          imageUrl:
-            "https://www.telegraph.co.uk/content/dam/Pets/spark/royal-canin/happy-puppy-xlarge.jpg?imwidth=1200",
-          left: 243,
-          top: 135,
-          width: 266,
-          height: 323,
-          angle: 355,
-          aspect: 0.823,
-          name: "Puppy 1",
-          id: "puppy1",
-          zIndex: 2,
-          pinAngle: 180,
-          pinColor: "red",
-          pinTop: "3%",
-          pinLeft: "calc(50% - 10px)",
-        },
-        {
-          type: "photo",
-          imageUrl:
-            "https://www.ardengrange.com/sites/admin/plugins/elfinder/files/ardengrange/Nutrition%20and%20Advice%20section/Fact%20Sheets%20section/Canine%20Fact%20Sheets/Puppy%20section/Puppy%20guide%20images/Scruffy%20pup.jpg",
-          left: 259,
-          top: 173,
-          width: 266,
-          height: 323,
-          angle: 25,
-          aspect: 0.823,
-          name: "Puppy 2",
-          id: "puppy2",
-          zIndex: 1,
-          pinAngle: 90,
-          pinColor: "green",
-          pinTop: "3%",
-          pinLeft: "calc(50% - 10px)",
-        },
-        {
-          type: "note",
-          text:
-            "Lorem ipsum dolor sit amet, usu mazim iuvaret in, ne eos virtute aliquid, nullam veritus imperdiet duo no. Iusto democritum at eam, pri omnis populo an. Id erat errem ullamcorper vel, vim in regione intellegat.",
-          left: 522,
-          top: 358,
-          width: 279,
-          height: 388,
-          angle: 5,
-          aspect: 0.72,
-          name: "Note",
-          id: "note1",
-          zIndex: 1,
-          pinAngle: 53,
-          pinColor: "yellow",
-          pinTop: "5%",
-          pinLeft: "calc(50% - 10px)",
-        },
-        {
-          type: "postit",
-          text: "Lorem ipsum dolor sit amet",
-          left: 525,
-          top: 146,
-          width: 181,
-          height: 179,
-          angle: 350,
-          aspect: 1.01,
-          name: "Post-It",
-          id: "postit1",
-          zIndex: 1,
-          pinAngle: 20,
-          pinColor: "red",
-          pinTop: "8%",
-          pinLeft: "calc(50% - 10px)",
-        },
-      ],
+      items: [],
       lines: [["item-puppy1", "item-puppy2"]],
     };
   }
@@ -392,6 +320,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    if (!localStorage.puppyState) this.writeData(resetData);
     this.readData();
   }
 
