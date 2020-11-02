1380f75ba692e752f314b27932985fc053e79b21
Nick <nick@dawncreative.co.uk>
2020-11-02 13:57:20 +0000

    tidy up utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,11 @@
 // Move item to top of pile (end of array)
 const moveToFront = (items, item) => {
-  // const thisIndex = items.map((i) => i.id).indexOf(item.id);
-  // const otherItems = items.filter((i) => i.id != item.id);
-  return [...items.filter((i) => i.id != item.id), item];
+  return [...items.filter((i) => i.id !== item.id), item];
 };
 
 // Move item to bottom of pile (beginning of array)
 const moveToBack = (items, item) => {
-  // const thisIndex = items.map((i) => i.id).indexOf(item.id);
-  // const otherItems = items.filter((i) => i.id != item.id);
-  return [item, ...items.filter((i) => i.id != item.id)];
+  return [item, ...items.filter((i) => i.id !== item.id)];
 };
 
 // module.exports = { moveToFront, moveToBack };
