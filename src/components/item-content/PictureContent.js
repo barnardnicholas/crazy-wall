cda64176ecf085dc7488d08ddd2b35feddd2e28b
Nick <nick@dawncreative.co.uk>
2020-11-02 14:02:49 +0000

    test .webp support

--- src/components/item-content/PictureContent.js
+++ src/components/item-content/PictureContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import polaroidFrame from "../../assets/img/polaroid-frame.png";
+import polaroidFrame from "../../assets/img/polaroid-frame.webp";
 import "./Items.css";
 import Pin from "./Pin";
 
